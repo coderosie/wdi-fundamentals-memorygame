335fa01b938b77d65d0086ef9502a3db19a4a714
Maritza Rhodas <Maritza@Maritzas-MacBook-Pro.local>
2018-01-25 00:57:52 -0800

    unit 8 commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 console.log("Up and running!");
-//var cardOne = "queen"; asked to delete the variables created in unit 7
-//var cardTwo = "king";
-//var cardThree = "queen";
-//var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = [i = 0];
-//unsure if above is create variable cardOne- store the 1st item in the cards array in the variable cardOne.
+var cardOne = cards[0];
+
 cardsInPlay.push(cardOne);
-//check above- add first card to the cardsInPlay array.  Use push method to add cardOne to end the cardsInPlay array.
+
 console.log("User flipped queen");
-var cardTwo = [i = 2];
+
+var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
+
 console.log("User flipped king");
 
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+} 
 
 
 
